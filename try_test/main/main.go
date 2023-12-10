79d9aff52fc039f5e70c5072e7da6e8d73b2e7c2
ledger <228664584@qq.com>
2023-12-10 16:18:33 +0800

    12

--- try_test/main/main.go
+++ try_test/main/main.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 )
 
 func main() {
-	// 定义命令行参数
-	methodPtr := flag.String("method", "defaultMethod", "指定要触发的方法")
-	// 添加其他命令行参数...
 
-	// 解析命令行参数
-	flag.Parse()
-	
-	// 调用相应的方法
-	switch *methodPtr {
-	case "method1":
-		method1()
-	case "method2":
-		method2()
-	default:
-		fmt.Println("未知的方法:", *methodPtr)
-	}
 }
 
 func method1() {
